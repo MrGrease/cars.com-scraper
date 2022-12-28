ee06653239175b20ee1c7c55fb81d307539410e4
mrgrease <mehmetemreyorulmaz@gmail.com>
2022-12-28 05:55:49 +0300

    Create browser and steps enum

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CefSharp.OffScreen;
+using CefSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,36 @@ namespace CarScraper
 {
     internal class Program
     {
+        enum Steps
+        {
+            Login,
+            EnterFirstSearch,
+            ScrapeFirstPageForModelS,
+            NextPage,
+            ScrapeLastPageForModelS,
+            SelectModelX,
+            ScrapeFirstPageForModelX,
+            Finish,
+            Fail
+        }
+        private static ChromiumWebBrowser _browser;
+        private static Steps _currentStep;
+        private static string _url = "https://www.cars.com/signin/";
         static void Main(string[] args)
         {
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+
+            var settings = new CefSettings()
+            {
+            };
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            _browser = new ChromiumWebBrowser(_url);
+            _browser.LoadingStateChanged += BrowserLoadingStateChanged;
+
+            Console.ReadKey();
+            Cef.Shutdown();
         }
-    }
+        private static void BrowserLoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+        }
 }
